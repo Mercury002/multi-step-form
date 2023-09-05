100838b30807b53dd10173b3ec516fb0114b28c5
Abduganiyev Sohibjon <abduganiyevsoxib002@gmail.com>
2023-09-05 16:43:18 +0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,90 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="main-section">
+      <div class="container-app">
+        <div class="main d-flex align-items-start">
+          <div class="main-tab nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist"
+            aria-orientation="vertical">
+            <div class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home"
+              type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
+              <span>1</span>
+              <div>
+                <p>Step 1</p>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+            <div class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile"
+              type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><span>2</span>
+              <div>
+                <p>Step 2</p>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+            <div class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages"
+              type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false"><span>3</span>
+              <div>
+                <p>Step 3</p>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+            <div class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings"
+              type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false"><span>4</span>
+              <div>
+                <p>Step 4</p>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+          </div>
+          <div class="main-form tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+              ...</div>
+            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...
+            </div>
+            <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...
+            </div>
+            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main-section {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+}
+
+.main {
+  width: 1000px;
+  height: 600px;
+  background-color: #fff;
+  border-radius: 15px;
+  padding: 10px;
+
+  &-tab {
+    width: 30%;
+    height: 100%;
+    border: 1px dashed blue;
+    border-radius: 10px;
+    background-color: #483eff;
+  }
+
+  &-form {
+    width: 70%;
+    height: 100%;
+    border: 1px dashed red;
+    border-radius: 10px;
+    padding: 20px 60px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
